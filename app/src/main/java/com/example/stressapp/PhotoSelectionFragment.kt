9093c805aada365153791ce5b4bfbcc5093a0931
Liam Thorne <lnthorne@shaw.ca>
2023-10-19 17:58:11 -0700

    Add resources and img view for selection

--- app/src/main/java/com/example/stressapp/PhotoSelectionFragment.kt
+++ app/src/main/java/com/example/stressapp/PhotoSelectionFragment.kt
@@ -9,23 +9,17 @@ import android.view.ViewGroup
 
 class PhotoSelectionFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PhotoSelectionFragment()
-    }
-
     private lateinit var viewModel: PhotoSelectionViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_photo_selection, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_photo_selection, container, false)
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PhotoSelectionViewModel::class.java)
-        // TODO: Use the ViewModel
+
+
+        return view
     }
 
 }
--- app/src/main/java/com/example/stressapp/PhotoSelectionViewModel.kt
+++ app/src/main/java/com/example/stressapp/PhotoSelectionViewModel.kt
@@ -1,7 +1,40 @@
 package com.example.stressapp
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class PhotoSelectionViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+typealias ImageResource = Int
+class PhotoSelectionViewModel(private val context: Context) : ViewModel() {
+    private val _currentPage = MutableLiveData<Int>(1)
+    val currentPage: LiveData<Int> get() = _currentPage
+
+    fun switchPage() {
+        _currentPage.value = (_currentPage.value ?: 1) % 3 + 1
+    }
+
+    fun getImagesForPage(): List<ImageResource> {
+        val arrayResourceId = when(_currentPage.value) {
+            1 -> R.array.page_1_images
+            2 -> R.array.page_2_images
+            3 -> R.array.page_3_images
+            else -> throw IllegalArgumentException("Invalid page number: ${_currentPage.value}")
+        }
+
+        val typedArray = context.resources.obtainTypedArray(arrayResourceId)
+        val imageResources = mutableListOf<ImageResource>()
+
+        for (i in 0 until typedArray.length()) {
+            imageResources.add(typedArray.getResourceId(i, -1))
+        }
+
+        typedArray.recycle()
+
+        return imageResources
+    }
+
+    fun selectImage(image: ImageResource ) {
+
+    }
 }
