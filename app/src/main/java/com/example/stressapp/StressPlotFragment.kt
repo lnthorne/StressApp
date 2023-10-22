003fa7954d79ef301f2424bbcae7f7bb2004f9ff
Liam Thorne <lnthorne@shaw.ca>
2023-10-21 18:05:00 -0700

    Display the stress data in the frag

--- app/src/main/java/com/example/stressapp/StressPlotFragment.kt
+++ app/src/main/java/com/example/stressapp/StressPlotFragment.kt
@@ -1,13 +1,22 @@
 package com.example.stressapp
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.charts.Cartesian
+import java.sql.Timestamp
+
 
 class StressPlotFragment : Fragment() {
 
@@ -16,19 +25,63 @@ class StressPlotFragment : Fragment() {
     }
 
     private lateinit var viewModel: StressPlotViewModel
-
+    private lateinit var anyChartView: AnyChartView
+    private lateinit var table: TableLayout
+    private lateinit var lineChart: Cartesian
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_stress_plot, container, false)
-        val btn: Button = view.findViewById(R.id.btnTest)
 
-        btn.setOnClickListener() {
-            Log.i("TestTag", StressDataController.getAllEntries().toString())
+        lineChart = AnyChart.line()
+        lineChart.xAxis(0).title("Instance")
+        lineChart.yAxis(0).title("Stress Level")
+        anyChartView = view.findViewById(R.id.any_chart_view)
+
+        table = view.findViewById(R.id.tlTable)
+
+        viewModel = ViewModelProvider(this).get(StressPlotViewModel::class.java)
+        viewModel.stressData.observe(viewLifecycleOwner) { stressDataList ->
+            setChartData(stressDataList)
+            setTableData(stressDataList)
         }
 
+
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.fetchStressData()
+    }
+
+    fun setChartData(stressDataList: List<StressData>) {
+        val chartData = stressDataList.mapIndexed { index, stressData ->
+            ValueDataEntry(index, stressData.stressLevel)
+        }
+        lineChart.data(chartData)
+
+        anyChartView.setChart(lineChart)
+    }
+
+    fun setTableData(stressDataList: List<StressData>) {
+        table.removeViews(1, table.childCount - 1)
+
+        for (stressData in stressDataList) {
+            val row = TableRow(context)
+            val timestampTextView = TextView(requireContext()).apply {
+                text = Timestamp(stressData.timestamp).toString()
+            }
+            val stressLevelTextView = TextView(requireContext()).apply {
+                text = stressData.stressLevel.toString()
+            }
+
+            row.addView(timestampTextView)
+            row.addView(stressLevelTextView)
+
+            table.addView(row)
+        }
     }
 
 }
--- app/src/main/java/com/example/stressapp/StressPlotViewModel.kt
+++ app/src/main/java/com/example/stressapp/StressPlotViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.stressapp
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StressPlotViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _stressData = MutableLiveData<List<StressData>>()
+    val stressData: LiveData<List<StressData>> get () = _stressData
+
+    fun fetchStressData() {
+        _stressData.value = StressDataController.getAllEntries()
+    }
 }
